38ef69655781531d8b5e6bcbbafe67f2b438b1b5
Marvs Garcia <garcia.marvs3@gmail.com>
2020-11-19 11:50:37 +0800

    adds scroll to top on every transition

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import Posts from './components/posts/Posts';
 import Post from './components/post/Post';
+import ScrollToTop from './utils/ScrollToTop';
 import './App.css';
 
 //Redux
